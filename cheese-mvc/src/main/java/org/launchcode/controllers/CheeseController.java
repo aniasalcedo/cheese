f08ddc461d5f750a33832a2c452b5fc69baec24f
Anna Salcedo <salcedoanna8@gmail.com>
2019-11-04 16:46:40 -0500

    Working on it

--- cheese-mvc/src/main/java/org/launchcode/controllers/CheeseController.java
+++ cheese-mvc/src/main/java/org/launchcode/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.Models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ import java.util.Map;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+//this is so we cant repeat the same NAME of the Cheese
+// (jesli uzyjemy tego samego name to zmieni sie tylko opis
+// ale nie bedziemy miec dwoch takich samych elementow)
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -38,13 +42,14 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName,
                                        @RequestParam String cheeseDescription) {
-        cheeses.put(cheeseName, cheeseDescription);
+        Cheese newCheese = new Cheese (cheeseName,cheeseDescription);
+        cheeses.add(newCheese);
         return "redirect:";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeses.keySet());
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "Remove Cheese");
         return "cheese/remove";
     }
